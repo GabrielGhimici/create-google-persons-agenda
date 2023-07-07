27897bbfed686dd23c27d7a5985da4c833a62734
GabrielGhimici <ghimici.gabriel@gmail.com>
2023-07-07 22:09:57 +0300

    add pdf parsing

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import { OutputPerson } from './src/output-person';
 import { InputPerson } from './src/input-person';
-import { createReadInterface, linesToList } from './src/read-input';
+import { createReadInterface, linesToList, readPDF } from './src/read-input';
 import { writeOutput } from './src/write-output';
 import { GROUP_SIZE } from './src/consts';
 
 async function main() {
-  const readInterface = createReadInterface('test.csv');
-  console.log('Reading from input/test.csv...');
-  const lines = await linesToList(readInterface);
+  //const readInterface = createReadInterface('test.csv');
+  //console.log('Reading from input/test.csv...');
+  //const lines = await linesToList(readInterface);
+  console.log('Reading from input/list.pdf...');
+  const lines = await readPDF('list.pdf');
   console.log(`${lines.length} row${lines.length === 1 ? '' : 's'} extracted...`);
   let groupIndex = 1;
   const result = lines.map((data, index) => {
--- src/read-input.ts
+++ src/read-input.ts
@@ -1,5 +1,7 @@
 import { createReadStream } from 'fs';
 import { Interface, createInterface } from 'readline';
+import { getDocument } from 'pdfjs-dist';
+import { TextItem } from 'pdfjs-dist/types/src/display/api';
 
 function createReadInterface(path: string) {
   const basePath = `input`;
@@ -22,4 +24,35 @@ function linesToList(readInterface: Interface): Promise<Array<string>> {
   });
 }
 
-export { createReadInterface, linesToList };
+async function readPDF(path: string) {
+  const basePath = `input`;
+  const internalPath = path.startsWith('/') ? path : `/${path}`;
+  const document = await getDocument(`${basePath}${internalPath}`).promise;
+  const numPages = document.numPages;
+  let list: Array<Array<string>> = [];
+
+  for (var i = 1; i <= numPages; i++) {
+    let pageList = await document
+      .getPage(i)
+      .then((pageData) => {
+        return pageData.getTextContent().then((textContent) => {
+          const curatedItems = textContent.items.filter((item) => {
+            const {str} = item as TextItem;
+            return str !== '' && str !== ' '
+          })
+          console.log(curatedItems)
+          return [];
+        });
+      })
+      .catch((err) => {
+        return [];
+      });
+
+    list = [...list, ...pageList];
+  }
+  document.destroy();
+  console.log(list);
+  return list;
+}
+
+export { createReadInterface, linesToList, readPDF };
